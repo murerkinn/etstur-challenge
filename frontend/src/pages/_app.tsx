2542bd18ce3adf260de534215b5ee978c47b5129
Murat Erkin Çiçek <murerkinn@gmail.com>
2022-06-20 00:19:19 +0300

    Use header and footer to create layout

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import '@/styles/index.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+
+import store from '@/app/store'
+import Footer from '@/components/footer'
+import Header from '@/components/header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Header />
+      <main className="container">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </Provider>
+  )
 }
 export default MyApp
