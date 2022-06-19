93bfdf0974b7a874ebf2de721047568845d4c5df
Murat Erkin Çiçek <murerkinn@gmail.com>
2022-06-20 00:17:59 +0300

    Create footer component

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,14 @@
+import Menu from './menu'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container footer-inner">
+        <p className="copyright-text">Event.ly | All Rights Reserved.</p>
+        <Menu />
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
