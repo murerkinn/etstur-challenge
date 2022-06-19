6d31bb3f7b67273135ecf7442e1a9306cb12e234
Murat Erkin Çiçek <murerkinn@gmail.com>
2022-06-19 20:33:51 +0300

    Create backend app

---
+++ backend/src/app.ts
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+import cors from 'cors'
+import express from 'express'
+import helmet from 'helmet'
+import Debug from 'debug'
+
+const debug = Debug('app')
+
+const PORT = process.env.PORT || '4000'
+
+const app = express()
+
+app.use(cors())
+app.use(helmet())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/health', (req, res) => res.json({ status: 'ok' }))
+
+app.listen(PORT, () => {
+  debug(`=================================`)
+  debug(`App listening on port ${PORT}...`)
+  debug(`=================================`)
+})
