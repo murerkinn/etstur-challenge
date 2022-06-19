f24423bf82273abd8090b532e154ef5f72f33b36
Murat Erkin Çiçek <murerkinn@gmail.com>
2022-06-20 00:17:12 +0300

    Create header component

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,46 @@
+import { useAppSelector } from '@/app/store'
+
+import Button from './button'
+import Logo from './logo'
+import Menu from './menu'
+import { openModal } from './modal'
+import ProfileInfo from './profile-info'
+
+const Header = () => {
+  const { user } = useAppSelector(state => state.auth)
+
+  return (
+    <header className="header">
+      <div className="container header-inner">
+        <Logo />
+
+        <nav>
+          <Menu />
+        </nav>
+
+        {user ? (
+          <ProfileInfo />
+        ) : (
+          <div className="btn-group">
+            <Button
+              className="invert"
+              size="small"
+              onClick={() => openModal('login')}
+            >
+              Login
+            </Button>
+            <Button
+              className="invert"
+              size="small"
+              onClick={() => openModal('register')}
+            >
+              Register
+            </Button>
+          </div>
+        )}
+      </div>
+    </header>
+  )
+}
+
+export default Header
