b992341b8a2b2008808997762fdd342cea1f38f5
Murat Erkin Çiçek <murerkinn@gmail.com>
2022-06-21 22:30:57 +0300

    Add `formatMoney` helper

---
+++ frontend/src/lib/format-money.ts
@@ -0,0 +1,5 @@
+export default function formatMoney(amount: number, currency = 'USD') {
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(
+    amount as number
+  )
+}
