dfa383d1318ab4a3997027ff8cc9d4c437938ec0
Murat Erkin Çiçek <murerkinn@gmail.com>
2022-06-21 11:05:28 +0300

    Add pagination and events data to search slice

--- frontend/src/modules/search/store/search-slice.ts
+++ frontend/src/modules/search/store/search-slice.ts
@@ -2,12 +2,19 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import { SearchStore } from './types'
 
+const searchParams = new URLSearchParams(global?.location?.search)
+
 const initialState: SearchStore = {
-  city: '',
-  startsAt: '',
-  endsAt: '',
-  query: '',
-  category: '',
+  city: searchParams.get('city') || '',
+  startsAt: searchParams.get('startsAt') || '',
+  endsAt: searchParams.get('endsAt') || '',
+  textSearch: searchParams.get('textSearch') || '',
+  category: searchParams.get('category') || '',
+  pageLimit: 10,
+  page: parseInt(searchParams.get('page') || '1'),
+  loading: false,
+  events: [],
+  totalPageCount: 1,
 }
 
 const searchSlice = createSlice({
@@ -24,15 +31,20 @@ const searchSlice = createSlice({
       state.startsAt = action.payload.startsAt
       state.endsAt = action.payload.endsAt
     },
-    setQuery(state: SearchStore, action: PayloadAction<string>) {
-      state.query = action.payload
+    setTextSearch(state: SearchStore, action: PayloadAction<string>) {
+      state.textSearch = action.payload
     },
     setCategory(state: SearchStore, action: PayloadAction<string>) {
       state.category = action.payload
     },
+    setPage(state: SearchStore, action: PayloadAction<number>) {
+      state.page = action.payload
+    },
+  },
   },
 })
 
-export const { setCity, setDates, setQuery, setCategory } = searchSlice.actions
+export const { setCity, setDates, setTextSearch, setCategory, setPage } =
+  searchSlice.actions
 
 export const searchReducer = searchSlice.reducer
--- frontend/src/modules/search/store/types.ts
+++ frontend/src/modules/search/store/types.ts
@@ -1,7 +1,28 @@
+type Category = {
+  _id: string
+  name: string
+}
+
+export type Event = {
+  _id: string
+  name: string
+  slug: string
+  organizator: string
+  organizatorName: string
+  startsAt: Date
+  endsAt: Date
+  category: Category
+}
+
 export type SearchStore = {
   city: string
   startsAt: string
   endsAt: string
-  query: string
+  textSearch: string
   category: string
+  pageLimit: number
+  page: number
+  loading: boolean
+  events: Event[]
+  totalPageCount: number
 }
