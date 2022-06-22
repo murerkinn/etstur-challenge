ea5a34c10622386176a25b651d0838e9dbfba0c1
Murat Erkin Çiçek <murerkinn@gmail.com>
2022-06-22 15:16:40 +0300

    Add social share buttons to tickets card

--- frontend/src/modules/event/components/tickets-card.tsx
+++ frontend/src/modules/event/components/tickets-card.tsx
@@ -1,5 +1,19 @@
 import ConfirmationNumberOutlinedIcon from '@mui/icons-material/ConfirmationNumberOutlined'
+import EmailIcon from '@mui/icons-material/Email'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import TelegramIcon from '@mui/icons-material/Telegram'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import startCase from 'lodash/startCase'
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from 'react-share'
 
 import { useAppSelector } from '@/app/store'
 // import Button from '@/components/button'
@@ -58,12 +72,56 @@ const TicketsCard = () => {
       )}
 
       {/* <Button
-        className="btn-block"
+        className="btn-block continue-button"
         variant="primary"
         disabled={tickets.length === 0}
       >
         Continue
       </Button> */}
+
+      <div className="social-buttons">
+        <EmailShareButton
+          url={window.location.href}
+          className="btn btn-icon-only"
+        >
+          <EmailIcon />
+        </EmailShareButton>
+
+        <FacebookShareButton
+          url={window.location.href}
+          className="btn btn-icon-only"
+        >
+          <FacebookOutlinedIcon />
+        </FacebookShareButton>
+
+        <LinkedinShareButton
+          url={window.location.href}
+          className="btn btn-icon-only"
+        >
+          <LinkedInIcon />
+        </LinkedinShareButton>
+
+        <TelegramShareButton
+          url={window.location.href}
+          className="btn btn-icon-only"
+        >
+          <TelegramIcon />
+        </TelegramShareButton>
+
+        <TwitterShareButton
+          url={window.location.href}
+          className="btn btn-icon-only"
+        >
+          <TwitterIcon />
+        </TwitterShareButton>
+
+        <WhatsappShareButton
+          url={window.location.href}
+          className="btn btn-icon-only"
+        >
+          <WhatsAppIcon />
+        </WhatsappShareButton>
+      </div>
     </div>
   )
 }
